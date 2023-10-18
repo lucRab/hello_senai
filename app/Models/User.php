eba665eba2f956f84f16b8138e6d5a54a5361120
guilherme <guilhermekeslley123@gmail.com>
2023-10-17 21:27:32 -0300

    update and generating username

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class User extends Model
 {
@@ -16,7 +17,7 @@ class User extends Model
         'senha'
     ];
     protected $table = "usuario";
-    protected $primaryKey = "idusuario";
+    protected $primaryKey = "apelido";
     const CREATED_AT = 'data_criacao';
     const UPDATED_AT = 'data_atualizacao';
     /**
@@ -24,20 +25,66 @@ class User extends Model
      * @param $data - 
     */
     public function createUser($data) {
-        
-        return $this->create($data);
+        $data['data_criacao'] = \Carbon\Carbon::now();
+        $id = $this->insertGetId($data);
+        $this->generateUsername($data['nome'], $id);
     }
     
     /**
-     * Função de atualização do Usuario
+     * Função de atualização do Usuario;
+     * @param $data  dados do usuario
+     * @param $id  id do usuario
+     * @return void
      */
-    public function updateUser() {
-        
+    public function updateUser($data,$id) {
+        return $this->where('idsuario',$id)->update($data);
+    }
+
+    /**
+     * Função para deletar o usuario;
+     * @param $id id do usuario
+     * @return void
+     */
+    public function deleteUser($id) {
+        return $this->where('idusuario', $id)->delete();
+    }
+
+    /**
+     * Função para gerar um apelido para o usuario;
+     * @param $name nome do usuario
+     * @param $user_id id do usuario
+     * @return void
+     */
+    public function generateUsername($name, $user_id) {
+        $username = $name . substr(sha1($name . $user_id), 0, 8);
+        $value = ['apelido' => $username];
+        DB::table('usuario')->where('idusuario', $user_id)->update($value);
+    }
+
+    /**
+     * Função para desativar a conta do usuario;
+     * @param $id id do usuario
+     * @return void
+     */
+    public function desativateUser($id): void {
+        return $this->where('idusuario',$id)->update(['status' => 'Desativado']);
     }
     
-    public function deleteUser() {
+    /**
+     * Função para selecionar o usuario pelo apelido;
+     * @param $nickname apelido do usuario
+     * @return array
+     */
+    public function getByNickname($nickname) {
+        return $this->where('apelido', $nickname)->get()->toArray();
+    }
 
-    }  
-
+    /**
+     * Função para selecionar todos os usuario;
+     * @return array
+     */
+    public function getAll() {
+        return $this->get()->toArray();
+    }
     use HasFactory;
 }
