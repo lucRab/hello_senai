b25673c6548cea77e418b9ae3c51ee07c2f9f51e
guilherme <guilhermekeslley123@gmail.com>
2023-10-27 17:31:45 -0300

    routes for auth and teacher

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\AuthController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -13,4 +15,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 // api/v1/usuario
 Route::group(['prefix' => 'v1'], function () {
     Route::apiResource('usuario', UserController::class);   
+    Route::apiResource('professor', TeacherController::class);   
+});
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/login', [AuthController::class, 'login']);   
+    Route::post('/logout', [AuthController::class, 'logout']);   
+    Route::post('/create/teacher', [AuthController::class, 'registerTeacher']);   
+    Route::get('/profile', [AuthController::class, 'profile']);   
 });
